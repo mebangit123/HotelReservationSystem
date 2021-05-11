da5d0715ae39c8d1a1e31c128100465675a9844d
mebangit123 <mebannongrum333@gmail.com>
2021-05-11 18:51:01 +0530

    Added welcome message

---
+++ HotelReservation/src/main/java/com/hotel/utils/HotelReservationSystem.java
@@ -0,0 +1,7 @@
+package com.hotel.utils;
+
+public class HotelReservationSystem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation program in HotelReservation class on Master Branch");
+	}
+}
